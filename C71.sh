b35f804592d258309d5c680bfaa726b9b9e35080
SealOne <gyg072507@gmail.com>
2017-11-01 16:44:25 +0800

    up
    up

---
+++ C71.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
+grub2-set-default 0
+
+sed -i '/\[main]/a\exclude=kernel*' /etc/yum.conf # 防止内核由于update产生变动
+
+echo
+read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
+if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
+    reboot
+else
+    exit
+fi
